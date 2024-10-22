376e8ee12b7c50dc854188387a163d7129a0e465
3xiic <122654327+3xiic@users.noreply.github.com>
2024-10-22 03:09:02 -0500

    Service Update

--- src/main/java/co/edu/unbosque/viajes_global_back/service/BookingDetailService.java
+++ src/main/java/co/edu/unbosque/viajes_global_back/service/BookingDetailService.java
@@ -1,17 +1,15 @@
 package co.edu.unbosque.viajes_global_back.service;
 
+import co.edu.unbosque.viajes_global_back.repository.BookingDetailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookingDetailService {
-
     @Autowired
-    @Lazy
-    private BookingDetailService bookingDetailService;
+    private BookingDetailRepository bookingDetailRepository;
 
-    public BookingDetailService() {
 
-    }
 }
+
+
--- src/main/java/co/edu/unbosque/viajes_global_back/service/NotificationChoiceService.java
+++ src/main/java/co/edu/unbosque/viajes_global_back/service/NotificationChoiceService.java
@@ -1,5 +1,6 @@
 package co.edu.unbosque.viajes_global_back.service;
 
+import co.edu.unbosque.viajes_global_back.repository.NotificationChoiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
@@ -8,8 +9,7 @@ import org.springframework.stereotype.Service;
 public class NotificationChoiceService {
 
     @Autowired
-    @Lazy
-    private NotificationChoiceService notificationChoiceService;
+    private NotificationChoiceRepository notificationChoiceRepository;
 
     public NotificationChoiceService() {
 
